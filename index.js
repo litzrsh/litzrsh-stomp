6bc47a67aa756a389f48202d7ce618461041f9ab
Jeeyoung Yoon <litzrsh@gmail.com>
2020-10-08 17:25:03 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+import SockJS from "sockjs-client";
+import Stomp from "stompjs";
+
+class Socket {
+    #url;
+    #debug = false;
+    #client;
+
+    constructor(url, debug) {
+        this.#url = url;
+        this.#debug = debug || false;
+        this.#client = undefined;
+
+        console.assert(this.#url, "URL can't be empty");
+    }
+
+    connect(headers) {
+        return new Promise((resolve, reject) => {
+            try {
+                let socket = new SockJS(this.#url);
+                this.#client = Stomp.over(socket);
+                this.#client.debug = this.#debug;
+                this.#client.connect(headers, frame => {
+                        resolve(frame);
+                    }, error => {
+                        reject(error);
+                    });
+            } catch (e) {
+                reject(e);
+            }
+        });
+    }
+
+    disconnect() {
+        try {
+            this.#client.disconnect();
+        } catch (e) {
+            console.error(e);
+        } finally {
+            this.#client = undefined;
+        }
+    }
+
+    subscribe(destination) {
+        return new Promise((resolve, reject) => {
+            try {
+                this.#client
+                    .subscribe(destination, message => {
+                        resolve(message);
+                    });
+            } catch (e) {
+                reject(e);
+            }
+        });
+    }
+}
+
+export default Socket;
